635407e46a033573145153efdae42e86f331f8e9
robertmryan <robert.ryan@mindspring.com>
2013-08-14 10:51:26 -0400

    Initial Commit

---
+++ XML Feed For Every Row Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XML Feed For Every Row Test
+//
+//  Created by Robert Ryan on 8/14/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XML Feed For Every Row Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XML Feed For Every Row Test
+//
+//  Created by Robert Ryan on 8/14/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
